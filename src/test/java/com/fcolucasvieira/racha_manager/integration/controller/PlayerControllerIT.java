88d0f13286c9f32d87be69c40d76edd4e762e22b
fcolucasvieira <fclucasvieira@gmail.com>
2026-05-30 10:08:51 -0300

    feat: implementação de teste de integração para PlayerController

---
+++ src/test/java/com/fcolucasvieira/racha_manager/integration/controller/PlayerControllerIT.java
@@ -0,0 +1,76 @@
+package com.fcolucasvieira.racha_manager.integration.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fcolucasvieira.racha_manager.application.dto.CreatePlayerRequest;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+public class PlayerControllerIT {
+    // Simula chamadas HTTP sem precisar subir servidor Tomcat
+    @Autowired
+    private MockMvc mockMvc;
+
+    // Utilizado para converter objetos Java em JSON
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Test
+    void shouldCreatePlayerSuccessfully() throws Exception {
+        CreatePlayerRequest request = new CreatePlayerRequest("Player");
+
+        mockMvc.perform(
+                post("/players")
+                        .contentType(APPLICATION_JSON)
+                        .content(
+                                objectMapper.writeValueAsString(request)
+                        )
+        )
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.success").value(true))
+                .andExpect(jsonPath("$.message").value("Player created successfully"))
+                .andExpect(jsonPath("$.data.id").exists())
+                .andExpect(jsonPath("$.timestamp").exists());
+
+    }
+
+    @Test
+    void shouldRejectBlankName() throws Exception {
+        CreatePlayerRequest request = new CreatePlayerRequest("");
+
+        mockMvc.perform(
+                post("/players")
+                        .contentType(APPLICATION_JSON)
+                        .content(
+                                objectMapper.writeValueAsString(request))
+        )
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.success").value(false))
+                .andExpect(jsonPath("$.message").exists())
+                .andExpect(jsonPath("$.timestamp").exists());
+    }
+
+    @Test
+    void shouldRejectNullName() throws Exception {
+        CreatePlayerRequest request = new CreatePlayerRequest(null);
+
+        mockMvc.perform(
+                post("/players")
+                        .contentType(APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request))
+        )
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.success").value(false))
+                .andExpect(jsonPath("$.message").exists())
+                .andExpect(jsonPath("$.timestamp").exists());
+    }
+}
